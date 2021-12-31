e0f9ef0e1d4cf54eb14b3179bcffb17463c6e10f
akshit.mithaiwala@techbulls.com <akshit.mithaiwala@techbulls.com>
2021-12-31 16:28:24 +0530

    changes in env

--- util/config.py
+++ util/config.py
@@ -3,7 +3,6 @@ from pydantic import BaseSettings
 
 # get enviroment variables
 class Settings(BaseSettings):
-    fastapi_db_hostname: str
     fastapi_db_port: str
     fastapi_db_username: str
     fastapi_db_password: str
